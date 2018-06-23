8603f240e11fbf5a2201d828f65826d540cc036b
Michael Fogleman <fogleman@gmail.com>
2018-06-22 21:34:08 -0400

    ignore png, remove util

--- util.go
+++
@@ -1,9 +0,0 @@
-package rush
-
-func updateOccupied(occupied []bool, stride, start, size int, value bool) {
-	idx := start
-	for i := 0; i < size; i++ {
-		occupied[idx] = value
-		idx += stride
-	}
-}
