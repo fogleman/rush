4868e8e6b1e9f05251cf86d2e366c05c7da9c292
Michael Fogleman <fogleman@gmail.com>
2018-06-30 23:57:30 -0400

    info to stderr

--- cmd/multi/main.go
+++ cmd/multi/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"runtime"
 	"time"
 
@@ -68,16 +69,24 @@ type Result struct {
 func worker(jobs <-chan EnumeratorItem, results chan<- Result) {
 	for job := range jobs {
 		board := job.Board
+
+		// only evaluate "canonical" boards
 		board.SortPieces()
 		if !IsCanonical(board) {
 			continue
 		}
+
+		// "unsolve" to find hardest reachable position
 		unsolver := NewUnsolverWithStaticAnalyzer(board, nil)
 		unsolved, solution := unsolver.UnsafeUnsolve()
 		unsolved.SortPieces()
+
+		// only interested in "non-trivial" puzzles
 		if solution.NumMoves < 2 {
 			continue
 		}
+
+		// if removing any piece does not affect the solution, skip
 		ok := true
 		for i := 1; i < len(unsolved.Pieces); i++ {
 			b := unsolved.Copy()
@@ -91,6 +100,8 @@ func worker(jobs <-chan EnumeratorItem, results chan<- Result) {
 		if !ok {
 			continue
 		}
+
+		// we are interested in this puzzle
 		results <- Result{board, unsolved, solution, job.Group, job.Counter, false}
 	}
 	results <- Result{Done: true}
@@ -132,10 +143,14 @@ func main() {
 		pct := float64(result.Counter) / MaxCounter
 		elapsed := time.Since(start)
 		fmt.Printf(
-			"%02d %02d %02d %s %.9f %d %s\n",
+			"%02d %02d %02d %s %d\n",
 			solution.NumMoves, solution.NumSteps, len(unsolved.Pieces), key,
-			pct, solution.MemoSize, elapsed)
+			solution.MemoSize)
+		fmt.Fprintf(
+			os.Stderr, "[%.9f] %d evaluated, %d distinct, %d groups - %s\n",
+			pct, count, len(seen), len(groups), elapsed)
 	}
+
 	fmt.Println(len(seen), count, len(groups))
 	// 4x4 = 31 31 31
 	// 5x5 = 2329 6073 2186
