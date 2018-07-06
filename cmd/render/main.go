7a7beef1b6baa9609525dd3755d170b922401831
Michael Fogleman <fogleman@gmail.com>
2018-07-06 16:51:32 -0400

    render cmd

---
+++ cmd/render/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/fogleman/gg"
+	"github.com/fogleman/rush"
+)
+
+func main() {
+	args := os.Args[1:]
+	if len(args) != 1 && len(args) != 2 {
+		fmt.Println("render DESC [OUTPUT]")
+	}
+
+	board, err := rush.NewBoardFromString(args[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	output := "out.png"
+	if len(args) == 2 {
+		output = args[1]
+	}
+	err = gg.SavePNG(output, board.Render())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
